b4d632a327ddae45e5520df46056a15ab0a5ca0b
michael <scienceopen@users.noreply.github.com>
2015-11-29 15:54:08 -0500

    init

---
+++ src/robust.h
@@ -0,0 +1,14 @@
+# define        DIM             512
+# define        DIMH            1024
+# define        FILTER          100
+# define        PI              3.141592654
+# define        ROUND(A)        ((A)<0.0?-((int)(-(A)+0.5)):((int)((A)+0.5)))
+# define        max(A,B)        ((A)<(B)?(B):(A))
+# define        min(A,B)        ((A)<(B)?(A):(B))
+# define        sign(A,B)        ((A)<0?-(B):(B))
+# define        abs(A)        ((A)<0?-(A):(A))
+
+#define ROOT2 1.4142136
+#define MAXLEN 256
+
+
