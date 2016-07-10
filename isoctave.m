27c97b4f90fb5c05870d2f0ab54f36f58aa787b7
scienceopen <scienceopen@users.noreply.github.com>
2016-07-09 22:42:01 -0400

    sync

--- isoctave.m
+++ isoctave.m
@@ -1,7 +1,10 @@
-function oct = isoctave()
-%Michael Hirsch Oct 2012
-% tested with Octave 3.6.3/4.0.0 and Matlab R2012a/R2015a
+function isoct = isoctave()
+persistent oct;
 
-oct = exist('OCTAVE_VERSION', 'builtin') == 5;
+if isempty(oct)
+    oct = exist('OCTAVE_VERSION', 'builtin') == 5;
+end
+
+isoct=oct; % has to be a separate line/variable for matlab
 
 end
