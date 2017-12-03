444d7a04a6c06ddde11fb910a2aee76e83397d53
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-03 01:24:23 -0500

    template

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
-req = ['nose','numpy','pillow','scipy']
-
-import pip
-try:
-    import conda.cli
-    conda.cli.main('install',*req)
-except Exception:
-    pip.main(['install'] + req)
+install_requires = ['numpy','pillow','scipy']
+tests_require=['nose','coveralls']
 #%%
 import subprocess
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='RobustFlow',
       version= '1.0.0',
-      packages=['robustflow'],
+      packages=find_packages(),
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scivision/robust-flow',
+      install_requires=install_requires,
+      extras_require={'tests':tests_require},
+      python_requires='>=3.5',
+      tests_require=tests_require,
 	  )
 
 subprocess.run(['cmake','../src'],cwd='bin')
